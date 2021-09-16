719ec2b44e9a49d53e90a7a409b0ff6a68be9483
Zander Baker <zanderbaker100@gmail.com>
2021-09-16 11:03:07 -0700

    Add StateMachine
    Create StateMachine, a simple class that allows for easy navigation of
    enum states

---
+++ Jammo.ParserTools/StateMachine.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Jammo.ParserTools
+{
+    public class StateMachine<TStates> : IEnumerable<TStates> where TStates : Enum
+    {
+        private readonly List<TStates> previous = new();
+
+        public TStates Current { get; private set; }
+
+        public StateMachine() { }
+        public StateMachine(TStates start)
+        {
+            Current = start;
+        }
+
+        public void MoveTo(TStates state)
+        {
+            previous.Add(Current);
+            Current = state;
+        }
+
+        public void MoveLast()
+        {
+            Current = previous.Last();
+            previous.Remove(Current);
+        }
+            
+        public TStates PeekLast()
+        {
+            return previous.Last();
+        }
+
+        public IEnumerator<TStates> GetEnumerator()
+        {
+            return previous.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
