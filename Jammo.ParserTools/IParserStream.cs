7c7350dd6c9fa97c6a5623eb48f669421223aec2
Zander Baker <zanderbaker100@gmail.com>
2021-09-15 21:12:16 -0700

    Add properties to IParserStream
    Add Isinitialized (bool) and FilePath (string) to IParserString

--- Jammo.ParserTools/IParserStream.cs
+++ Jammo.ParserTools/IParserStream.cs
@@ -4,6 +4,9 @@ namespace Jammo.ParserTools
 {
     public interface IParserStream : IDisposable
     {
+        public bool IsInitialized { get; }
+        public string FilePath { get; }
+    
         public void Parse();
         public void Write();
         public void WriteTo(string path);
